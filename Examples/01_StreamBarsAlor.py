0d7ff42b02ef5f34a2e5aa8f68816fbff2e0dbeb
cia76 <ichechet@mail.ru>
2023-06-29 12:04:57 +0500

    seconds -> total_seconds()

--- Examples/01_StreamBarsAlor.py
+++ Examples/01_StreamBarsAlor.py
@@ -10,17 +10,17 @@ if __name__ == '__main__':  # Точка входа при запуске это
     exchange = 'MOEX'  # Биржа 'MOEX' или 'SPBX'
     # symbol = 'SBER'  # Тикер
     symbol = 'SiU3'  # Формат фьючерса: <Тикер><Месяц экспирации><Последняя цифра года> Месяц экспирации: 3-H, 6-M, 9-U, 12-Z
-    # time_frame = timedelta(minutes=1)  # 1 минута
+    time_frame = timedelta(minutes=1)  # 1 минута
     # time_frame = timedelta(minutes=5)  # 5 минут
     # time_frame = timedelta(minutes=15)  # 15 минут
     # time_frame = timedelta(hours=1)  # 1 час
-    time_frame = timedelta(days=1)  # 1 день
+    # time_frame = timedelta(days=1)  # 1 день
     delta = timedelta(seconds=3)  # Задержка перед запросом нового бара
     # schedule = MOEXStocks()  # Расписание фондового рынка Московской биржи
     schedule = MOEXFutures()  # Расписание срочного рынка Московской биржи
 
     ap_provider = AlorPy(Config.UserName, Config.RefreshToken)  # Провайдер работает со счетом по токену (из файла Config.py) Подключаемся к торговому счету
-    tf = 'D' if time_frame == timedelta(days=1) else 'W' if time_frame == timedelta(weeks=1) else time_frame.seconds  # Временной интервал для дневок, неделек и интрадея
+    tf = 'D' if time_frame == timedelta(days=1) else 'W' if time_frame == timedelta(weeks=1) else time_frame.total_seconds()  # Временной интервал для дневок, неделек и интрадея
     while True:
         market_datetime_now = schedule.utc_to_msk_datetime(datetime.utcnow())  # Текущее время на бирже
         print('\nТекущее время на бирже:', market_datetime_now)
@@ -31,8 +31,8 @@ if __name__ == '__main__':  # Точка входа при запуске это
         trade_bar_request_datetime = schedule.get_trade_bar_request_datetime(trade_bar_open_datetime, time_frame)  # Дата и время запроса бара на бирже
         print('Время запроса бара:', trade_bar_request_datetime)
         sleep_time = trade_bar_request_datetime - market_datetime_now + delta  # Время ожидания
-        print('Одижание в секундах:', sleep_time.seconds)
-        sleep(sleep_time.seconds)  # Ждем
+        print('Одижание в секундах:', sleep_time.total_seconds())
+        sleep(sleep_time.total_seconds())  # Ждем
         bars = ap_provider.get_history(exchange, symbol, tf, seconds_from)['history']  # Получаем последний сформированный и текущий несформированный (если имеется) бары
         # print(bars)
         if len(bars) == 0:  # Если бары не получены
